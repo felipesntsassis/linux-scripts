6664845986ba68a2840333e96bdbd9c7af05b1e4
Felipe dos Santos Assis <felipesntsassis@gmail.com>
2020-02-04 10:03:54 -0300

    Update ubuntu-update.sh
    Melhorias dos scripts.

--- ubuntu-update.sh
+++ ubuntu-update.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 
-echo "*******************     SETUP DEVELOPMENT ENVIROMENT - UBUNTU     *******************";
-sudo apt update
-sudo apt upgrade
-
-# Incluir repositórios em /etc/apt/sources.list
-sudo echo "# Google Repository" >> /etc/apt/sources.list
-sudo echo "deb http://dl.google.com/linux/deb/ stable main" >> /etc/apt/sources.list
-
-# Criar chaves
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo "*******************     SETUP DEVELOPMENT ENVIROMENT - UBUNTU     *******************"
 
 # Atualizção de Pacotes
 sudo apt update
@@ -28,14 +19,18 @@ sudo apt install -y clementine
 sudo apt install -y brasero
 sudo apt install -y cdrdao
 sudo apt install -y usb-creator-gtk
-sudo apt install -y gimp
+sudo apt install -y gimp gimp-plugin-registry gimp-data-extras
 sudo apt install -y inkscape
 sudo apt install -y shotwell
 sudo apt install -y ttf-mscorefonts-installer
-sudo apt install -y google-chrome-stable
 
 # Aplicações de Desenvolvimento
 sudo apt install -y git
+sudo apt install -y tilix
+# Configurar o Tilix como terminal padrão
+sudo update-alternatives --config x-terminal-emulator
+sudo apt install -y mariadb-server
+
 sudo apt install -y zsh
 chsh -s $(which zsh)
 
@@ -44,14 +39,12 @@ sudo snap install --classic skype
 sudo snap install spotify
 
 # Criar ambiente de desenvolvimento
-mkdir -p ~/desenv/java
-mkdir -p ~/desenv/cabonnet
-mkdir -p ~/desenv/escolpi
-mkdir -p ~/desenv/rhf
-mkdir -p ~/desenv/escolpi
+mkdir -p ~/desenv/projetos/cabonnet
+mkdir -p ~/desenv/projetos/escolpi
+mkdir -p ~/desenv/projetos/rhf
 
-# Aplicações de desenvolvimento
-
+git config --global user.name "Felipe dos Santos Assis"
+git config --global user.email "felipesntsassis@gmail.com"
 
 echo "Instalação finalizada com sucesso! Tenha um bom dia ;-)"
 echo "*************************************************************************************"
